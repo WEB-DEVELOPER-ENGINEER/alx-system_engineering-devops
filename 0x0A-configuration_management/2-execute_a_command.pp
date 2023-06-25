2517baaa299b9bce323c4013dd2c196e5534142c
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-06-25 07:25:40 -0700

    done

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
-# kills a process named killmenow
+# kills a process named killemenow
 exec { 'pkill killmenow':
-    command => 'pkill -f killmenow',
+  command => 'pkill -f killmenow',
+  path    => '/usr/bin/'
 }
