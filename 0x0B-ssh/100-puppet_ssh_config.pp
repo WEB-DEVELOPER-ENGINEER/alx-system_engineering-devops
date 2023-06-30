57e9ff666639c8e5e8e8ada79a2f95a435a1f2cd
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-06-30 07:08:21 -0700

    done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,4 @@
+# Puppet
 exec { 'ssh_config':
   path    => '/bin',
   command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
