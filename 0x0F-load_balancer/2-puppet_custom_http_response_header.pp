c3761c054ab6e3c6685b6e5f79dd9e41bf157a3b
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-07-09 14:40:41 -0700

    done

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,34 +1,20 @@
 # Automate the task of creating a custom HTTP header
 
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package {'nginx':
+  ensure => 'present',
+}
 class { 'nginx':
   ensure => running,
 }
 
-package { 'curl':
-  ensure => 'present',
-}
-
 file { '/etc/nginx/custom_headers.conf':
   content => "add_header X-Served-By \"$hostname\";",
-  owner   => 'root',
-  group   => 'root',
   mode    => '0644',
 }
 
-file { '/etc/nginx/nginx.conf':
-  ensure => 'file',
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0644',
-  content => template('my_module/nginx.conf.erb'),
-  notify => Exec['nginx_reload'],
-}
-
-exec { 'nginx_reload':
-  command     => '/usr/sbin/service nginx reload',
-  refreshonly => true,
-  subscribe   => [
-    File['/etc/nginx/custom_headers.conf'],
-    File['/etc/nginx/nginx.conf'],
-  ],
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
