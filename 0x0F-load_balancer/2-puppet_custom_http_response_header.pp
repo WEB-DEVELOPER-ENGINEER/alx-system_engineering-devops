c65f2dff6893fde3c3db6cc4a290e7cbb681e354
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-07-09 14:41:31 -0700

    done

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,15 +6,11 @@ exec {'update':
 -> package {'nginx':
   ensure => 'present',
 }
-class { 'nginx':
-  ensure => running,
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-file { '/etc/nginx/custom_headers.conf':
-  content => "add_header X-Served-By \"$hostname\";",
-  mode    => '0644',
-}
-
 -> exec {'run':
   command => '/usr/sbin/service nginx restart',
 }
