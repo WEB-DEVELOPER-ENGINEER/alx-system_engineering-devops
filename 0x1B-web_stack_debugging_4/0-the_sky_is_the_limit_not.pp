fe5ccd72d9667a73472ef8f23bc6a8a77acb9b38
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-09-01 03:16:08 -0700

    done

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# puppet code to fix nginx server
+
+exec { 'sed -i "s/15/4096/" /etc/default/nginx':
+  cwd  => '/root',
+  path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+}
+
+exec { 'sudo service nginx restart':
+  cwd  => '/root',
+  path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+}
